508154fda2c9640366a7f05509201314b0c3a103
MaheshBBhor <maheshbhor97@gmail.com>
2024-08-09 22:35:45 +0530

    test

--- src/component/Login.js
+++ src/component/Login.js
@@ -16,28 +16,26 @@ function Login({ onLogin }) {
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    // Validate the username and password here
-    const { username, password } = formData;
-    const usersData = require("../users.json");
-    const users = usersData.users;
+    // e.preventDefault();
+    // const { username, password } = formData;
+    // const usersData = require("../users.json");
+    // const users = usersData.users;
 
-    const foundUser = users.find(
-      (user) => user.username === username && user.password === password
-    );
+    // const foundUser = users.find(
+    //   (user) => user.username === username && user.password === password
+    // );
 
-    if (foundUser) {
+    // if (foundUser) {
       onLogin();
-      //   alert("Login successful!");
-    } else {
-      alert("Login failed. Please check your credentials.");
-    }
+    // } else {
+    //   alert("Login failed. Please check your credentials.");
+    // }
   };
 
   return (
     <div className="flex items-center justify-center h-screen bg-pink-100">
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-        <h2 className="text-2xl font-bold mb-6 text-center">Login To Web</h2>
+        <h2 className="text-2xl font-bold mb-6 text-center">Login To React App</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
           <div className="flex flex-col">
             <label className="text-sm font-medium mb-1" htmlFor="username">Username</label>
@@ -47,7 +45,7 @@ function Login({ onLogin }) {
               name="username"
               value={formData.username}
               onChange={handleChange}
-              placeholder="Enter your username"
+              placeholder="Enter your name"
               className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               required
             />
@@ -60,7 +58,7 @@ function Login({ onLogin }) {
               name="password"
               value={formData.password}
               onChange={handleChange}
-              placeholder="Enter your password"
+              placeholder="Enter any password"
               className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               required
             />
